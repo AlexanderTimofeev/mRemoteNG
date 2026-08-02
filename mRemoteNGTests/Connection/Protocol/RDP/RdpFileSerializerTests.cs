852e62873d3755600393bbaab3a5dac7793e6288
sanay <sanay.sotis@gmail.com>
2026-08-02 23:42:23 +0300

    test: cover native RDP sizing serialization

--- mRemoteNGTests/Connection/Protocol/RDP/RdpFileSerializerTests.cs
+++ mRemoteNGTests/Connection/Protocol/RDP/RdpFileSerializerTests.cs
@@ -35,6 +35,52 @@ namespace mRemoteNGTests.Connection.Protocol.RDP
             Assert.That(result, Does.Not.Contain("password 51"));
         }
 
+        [Test]
+        public void SerializeMapsPredefinedResolution()
+        {
+            ConnectionInfo connectionInfo = new()
+            {
+                Hostname = "rdp.example.test",
+                Resolution = RDPResolutions.Res1920x1080
+            };
+
+            string result = RdpFileSerializer.Serialize(connectionInfo);
+
+            Assert.That(result, Does.Contain("desktopwidth:i:1920"));
+            Assert.That(result, Does.Contain("desktopheight:i:1080"));
+        }
+
+        [Test]
+        public void SerializeMapsCustomResolution()
+        {
+            ConnectionInfo connectionInfo = new()
+            {
+                Hostname = "rdp.example.test",
+                Resolution = RDPResolutions.Custom,
+                ResolutionWidth = 1720,
+                ResolutionHeight = 980
+            };
+
+            string result = RdpFileSerializer.Serialize(connectionInfo);
+
+            Assert.That(result, Does.Contain("desktopwidth:i:1720"));
+            Assert.That(result, Does.Contain("desktopheight:i:980"));
+        }
+
+        [Test]
+        public void SerializeEnablesSmartSizing()
+        {
+            ConnectionInfo connectionInfo = new()
+            {
+                Hostname = "rdp.example.test",
+                Resolution = RDPResolutions.SmartSize
+            };
+
+            string result = RdpFileSerializer.Serialize(connectionInfo);
+
+            Assert.That(result, Does.Contain("smart sizing:i:1"));
+        }
+
         [TestCase(RDPDesktopScaleFactor.Scale100, 100)]
         [TestCase(RDPDesktopScaleFactor.Scale125, 125)]
         [TestCase(RDPDesktopScaleFactor.Scale150, 150)]
